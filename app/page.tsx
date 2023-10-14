25cf2ba6551eff9f0d2a2a046bf7660dab59341d
Luiz Lima <110183537+luizgolima@users.noreply.github.com>
2023-10-14 11:15:53 -0300

    feat: Impl. responsividade

--- app/page.tsx
+++ app/page.tsx
@@ -5,56 +5,58 @@ export default function Home() {
   return (
       <>
           <Header />
-          <main className="h-screen w-full bg-gray-50
-                           flex flex-col items-center">
-            <header style={{ backgroundImage: `url(${bgImage.src})` }}
-                    className="w-screen bg-center bg-cover flex flex-col justify-center items-center mb-8 py-20">
-                <h1 className="text-6xl font-bold mb-2 bg-green-800 border-2 border-gray-200 opacity-90 text-gray-50 py-4 px-8 rounded">Embrace ESG</h1>
-                <h3 className="text-2xl font-bold">Junte-se a esta causa</h3>
-            </header>
+          <main className="w-full bg-gray-50 flex flex-col items-center">
+              <header style={{backgroundImage: `url(${bgImage.src})`}}
+                      className="w-screen bg-center bg-cover flex flex-col justify-center items-center mb-8 py-20">
+                  <h1 className="bg-green-800 opacity-90 rounded border-2 border-gray-200
+                                 font-bold text-2xl lg:text-6xl text-gray-50 mb-2 py-4 px-8">
+                      Embrace ESG
+                  </h1>
+                  <h3 className="lg:text-2xl font-bold">Junte-se a esta causa</h3>
+              </header>
               <div aria-label="Area de cards"
-                   className="w-1/2 flex justify-center items-center gap-4">
+                   className="w-1/2 sm:w-screen sm:px-8 flex flex-col sm:flex-row justify-center items-center gap-4 mb-8">
                   <section
                       aria-label="Card de pontos"
                       title="Veja quantos pontos você tem"
-                      className="h-[400px] w-[600px] shadow-lg rounded3 p-4 hover:scale-105 active:scale-100
+                      className="h-[350px] w-[300px] lg:h-[400px] lg:w-1/3 shadow-lg rounded3 p-4 hover:scale-105 active:scale-100
                                       transition-all flex flex-col justify-center items-center">
                       <p className="text-gray-950 text-center mb-4">Você ainda não tem pontos</p>
                       <h3 className="text-9xl text-gray-950 text-center mb-4">0</h3>
                       <button className="bg-[#F27E63] hover:bg-[#F27E64] hover:scale-105 active:scale-100
-                                         transition-all text-gray-950 font-bold py-3 px-6 rounded">
-                          Entrar ou fazer cadastro
+                                         transition-all text-gray-950 font-bold py-2 px-4 rounded">
+                          Entrar ou cadastre-se
                       </button>
                   </section>
                   <section
                       aria-label="Card explicando como funciona o sistema de pontuação"
                       title="Veja como funciona o sistema de pontuação"
-                      className="h-[400px] w-[600px] shadow-lg rounded3 p-4 hover:scale-105 active:scale-100
+                      className="h-[350px] w-[300px] lg:h-[400px] lg:w-1/3 shadow-lg rounded3 p-4 hover:scale-105 active:scale-100
                                       transition-all flex flex-col justify-center items-center">
-                      <h3 className="text-3xl text-gray-950 text-center mb-4">
+                      <h3 className="font-bold lg:text-3xl text-gray-950 text-center mb-4">
                           Sistema de pontuação
                       </h3>
                       <p className="w-4/5 text-gray-950 text-center mb-4">
                           Ganhe pontos ao participar de atividades ESG e troque por brindes
                       </p>
                       <button className="bg-[#F27E63] hover:bg-[#F27E64] hover:scale-105 active:scale-100
-                                         transition-all text-gray-950 font-bold py-3 px-6 rounded">
+                                         transition-all text-gray-950 font-bold py-2 px-4 rounded">
                           Veja como funciona
                       </button>
                   </section>
                   <section
                       aria-label="Card para ver histórias de participantes"
                       title="Veja as histórias dos participantes do programa"
-                      className="h-[400px] w-[600px] shadow-lg rounded3 p-4 hover:scale-105 active:scale-100
+                      className="h-[350px] w-[300px] lg:h-[400px] lg:w-1/3 shadow-lg rounded3 p-4 hover:scale-105 active:scale-100
                                       transition-all flex flex-col justify-center items-center">
-                      <h3 className="font-bold text-2xl text-gray-950 text-center mb-4">
-                          Veja as histórias dos participantes do programa
+                      <h3 className="font-bold lg:text-3xl text-gray-950 text-center mb-4">
+                          Histórias dos participantes do programa
                       </h3>
                       <p className="w-4/5 text-gray-950 text-center mb-4">
                           O nosso programa conta com 1000+ participantes que já estão fazendo a diferença
                       </p>
                       <button className="bg-[#F27E63] hover:bg-[#F27E64] hover:scale-105 active:scale-100
-                                         transition-all text-gray-950 font-bold py-3 px-6 rounded">
+                                         transition-all text-gray-950 font-bold py-2 px-4 rounded">
                           Veja suas histórias
                       </button>
                   </section>
--- app/shared/Header.tsx
+++ app/shared/Header.tsx
@@ -1,24 +1,59 @@
+'use client'
 import Link from "next/link";
-
+import { useState } from "react";
 
 export default function () {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const toggleMenu = () => {
+      setMenuOpen(!menuOpen);
+  };
+
   return (
-    <header className="h-28 flex justify-around items-center bg-[#F27E63] text-black">
-        <h1 className="text-3xl font-bold py-2 px-4 bg-green-800 text-white rounded border-2 border-gray-200">
+    <header className="h-28 w-screen px-4 lg:px-0 flex justify-between lg:justify-around items-center bg-[#F27E63] text-black">
+        <h1 className="lg:text-3xl font-bold py-2 px-4
+                        bg-green-800 text-gray-50 rounded border-2 border-gray-200">
             <Link href="/">Embrace ESG</Link>
         </h1>
-        <nav>
-            <ul className="flex justify-around items-center gap-4">
-                <li><Link href="/" className="text-gray-950">Horta Doméstica</Link></li>
-                <li><Link href="/" className="text-gray-950">Reciclagem</Link></li>
-                <li><Link href="/" className="text-gray-950">Alimentação Saudável</Link></li>
-                <li><Link href="/" className="text-gray-950">Atividades Físicas</Link></li>
-                <li><Link href="/" className="text-gray-950">Voluntariado</Link></li>
+        <nav className={`lg:flex gap-4 ${menuOpen ? 'block' : 'hidden'}`}>
+            <ul className="h-[100svh] lg:h-auto w-full lg:w-auto absolute top-28 left-16 lg:static lg:top-0
+                                   flex flex-col lg:flex-row bg-green-800 lg:bg-[#F27E63] gap-4 z-10">
+                <li className="mx-4 lg:mx-auto mt-4 lg:mt-auto">
+                    <Link href="/" className="hover:underline text-gray-50 lg:text-gray-950">
+                        Horta Doméstica
+                    </Link>
+                </li>
+                <li className="mx-4 lg:mx-auto mt-1 lg:mt-auto">
+                    <Link href="/" className="hover:underline text-gray-50 lg:text-gray-950">
+                        Reciclagem
+                    </Link>
+                </li>
+                <li className="mx-4 lg:mx-auto mt-1 lg:mt-auto">
+                    <Link href="/" className="hover:underline text-gray-50 lg:text-gray-950">
+                        Alimentação Saudável
+                    </Link>
+                </li>
+                <li className="mx-4 lg:mx-auto mt-1 lg:mt-auto">
+                    <Link href="/" className="hover:underline text-gray-50 lg:text-gray-950">
+                        Atividades Físicas
+                    </Link>
+                </li>
+                <li className="mx-4 lg:mx-auto mt-1 lg:mt-auto">
+                    <Link href="/" className="hover:underline text-gray-50 lg:text-gray-950">
+                        Voluntariado
+                    </Link>
+                </li>
             </ul>
         </nav>
-        <button className="bg-white hover:bg-gray-200 hover:scale-105 active:scale-100
+        <button className="hidden lg:block bg-white hover:bg-gray-200 hover:scale-105 active:scale-100
                            transition-all text-gray-950 font-bold py-3 px-6 rounded">
             Entrar
+        </button>
+        <button
+            id="menu-button"
+            className="lg:hidden text-gray-50 lg:text-gray-950 text-4xl focus:outline-none"
+            onClick={toggleMenu}
+        >
+            ☰
         </button>
     </header>
   )
